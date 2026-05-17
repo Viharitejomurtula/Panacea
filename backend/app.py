54ac35b80e6600ebcfcba43a7e5638f9b2de6b35
Viharitejomurtula <viharitejo@gmail.com>
2026-05-16 21:28:06 -0700

    changed some CORS origin thigns

--- backend/app.py
+++ backend/app.py
@@ -53,6 +53,7 @@ app = FastAPI(title="Panacea API", version="0.2.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
